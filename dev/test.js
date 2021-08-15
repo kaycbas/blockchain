c9b4d74d6b9238d01f20f86c2bcdcf3429d18182
Kevin Bastoul <kaycbas@gmail.com>
2021-08-14 17:49:56 -0700

    test hashBlock method

--- dev/test.js
+++ dev/test.js
@@ -2,11 +2,33 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.creatteNewBlock(652754, '8TY76G5Y7VU45VB', 'C98VB7N67V8CVBN');
+// bitcoin.creatteNewBlock(652754, '8TY76G5Y7VU45VB', 'C98VB7N67V8CVBN');
 
-bitcoin.createNewTransaction(100, 'ALEX9S0AF870DSA', 'JENNK3L25L2K3JH5');
-bitcoin.createNewTransaction(99, 'TORI9S0AF870DSA', 'NICKK3L25L2K3JH5');
+// bitcoin.createNewTransaction(100, 'ALEX9S0AF870DSA', 'JENNK3L25L2K3JH5');
+// bitcoin.createNewTransaction(99, 'TORI9S0AF870DSA', 'NICKK3L25L2K3JH5');
 
-bitcoin.creatteNewBlock(534334, '6JK5L6G5Y7VU45VB', '5N4MB7N67V8CVBN');
+// bitcoin.creatteNewBlock(534334, '6JK5L6G5Y7VU45VB', '5N4MB7N67V8CVBN');
 
-console.log(bitcoin.chain[1].transactions);
+const prevBlockHash = '4GH5KJ32LH234J12LKJ';
+const currBlockData = [
+    {
+        amount: 10,
+        sender: 'H54JK32L243JKLJJ',
+        recipient: '6VBMN345234M5NB'
+    },
+    {
+        amount: 20,
+        sender: 'j45k32K32L243JKLJJ',
+        recipient: '5kl43MN345234M5NB'
+    },
+    {
+        amount: 30,
+        sender: 'BNM5JK32L243JKLJJ',
+        recipient: '6BN5MN345234M5NB'
+    },
+];
+const nonce = 4322;
+
+hash = bitcoin.hashBlock(prevBlockHash, currBlockData, nonce)
+
+console.log(hash);
